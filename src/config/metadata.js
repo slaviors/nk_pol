22f009abe69f8af0ea0704f4893bd4b1d771d2d9
mamatqurtifa <mqurtifawijaya007@gmail.com>
2025-10-29 19:22:34 +0700

    fix: update metadata description for clarity and conciseness

--- src/config/metadata.js
+++ src/config/metadata.js
@@ -1,6 +1,6 @@
 export const siteMetadata = {
   title: 'NK POL - Kontraktor Stand Pameran Profesional & Terpercaya',
-  description: 'NK POL adalah kontraktor pameran terpercaya sejak 2019. Kami menyediakan jasa desain, konstruksi, dan instalasi booth pameran berkualitas dengan harga kompetitif.',
+  description: 'NK POL adalah kontraktor pameran terpercaya sejak 2019 yang menyediakan jasa desain, konstruksi, dan instalasi booth berkualitas dengan harga kompetitif.',
   author: 'NK POL',
   siteUrl: 'https://www.nkpol.com',
   locale: 'id_ID',
